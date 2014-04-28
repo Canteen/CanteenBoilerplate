b8a14a93b0b300a47ba2514a7a08fa8eb935b94f
Matt Karl <matt@mattkarl.com>
2014-04-28 00:10:05 -0400

    Updated to Canteen 1.2.1

--- deploy/index.php
+++ deploy/index.php
@@ -5,14 +5,6 @@
 
 	// Setup the site
 	$site = new Canteen\Site();
-
-	// Cache busting to the render to cache bust local assets
-	// use {{cacheBust}} after a file name, like JS or CSS files
-	$site->settings->addSetting(
-		'cacheBust', 
-		'?'.($site->settings->local ? 'cb='.time() : 'v=1'),
-		SETTING_RENDER | SETTING_CLIENT
-	);
 	
 	// Echo out the page result
 	$site->render();
